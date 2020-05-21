18a1ce94fc0275ab66591b8aca1403f2f964b1a8
PRAMOD P M <53473387+pramodpm2@users.noreply.github.com>
2020-05-21 18:14:56 +0530

    Update Swapping.py

--- Swapping.py
+++ Swapping.py
@@ -1,3 +1,12 @@
+'''
+Write Python Program to Reverse a Given Number
+This is a Python Program to reverse a given number.
+Problem Description
+The program takes a number and reverses it and store it in another variable and show it
+'''
+
+
+
 x=int(input('Enter x: '))
 y=int(input('Enter y: '))
 
